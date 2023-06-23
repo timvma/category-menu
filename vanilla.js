263e7213212cf6ba96da81ebdc9d98a6b8421101
timvma <timawbi@gmail.com>
2023-06-23 18:57:42 +0900

    up

--- vanilla.js
+++ vanilla.js
@@ -4,12 +4,20 @@ const menu = document.getElementById('menu');
 const menuLinksContainer = document.querySelector('.menu__container');
 
 const menus = [
-  { parent: "main", handle: "basketball", name: "Basketball" },
+  { parent: "main", handle: "basketball", name: "BASKETBALL" },
   { parent: "basketball", handle: "basketball-ball", name: "Ball" },
   { parent: "basketball", handle: "basketball-bag", name: "Bag" },
   { parent: "basketball-ball", handle: "basketball-ball-5", name: "Size 5", link: "/" },
   { parent: "basketball-bag", handle: "basketball-bag-30l", name: "30L", link: "/" },
-  { parent: "main", handle: "soccer", name: "Soccer" },
+  { parent: "basketball-bag", handle: "basketball-bag-big", name: "Big" },
+  { parent: "basketball-bag", handle: "basketball-bag-small", name: "Small"},
+  { parent: "basketball-bag-small", handle: "basketball-bag-small-15", name: "15L"},
+
+  { parent: "main", handle: "soccer", name: "SOCCER" },
+  { parent: "main", handle: "volleyball", name: "VOLLEYBALL" },
+  { parent: "main", handle: "handball", name: "HANDBALL" },
+  { parent: "main", handle: "others", name: "OTHERS" },
+
   { parent: "soccer", handle: "soccer-ball", name: "Ball" },
   { parent: "soccer-ball", handle: "soccer-ball-5", name: "30L", link: "/" }
 ];
@@ -30,23 +38,28 @@ function back() {
   }
 }
 
-function updateMenu() {
+async function updateMenu() {
   const currentMenuHandle = history[history.length - 1];
 
-  if (currentMenuHandle === 'main') {
+  if (currentMenuHandle === 'main') 
     menu.classList.remove('menu--hide');
-  } else {
+  else 
     menu.classList.add('menu--hide');
-  }
- 
+  
+ let hello = setTimeout(() => {
+  
+  }, 5000);
+  
+  let wee = await hello;
+
   menuLinksContainer.innerHTML = '';
 
   const fragment = document.createDocumentFragment();
   const currentLists = menus.filter(menu => menu.parent === currentMenuHandle);
-
+  let delay = 1
   currentLists.forEach(({ name, link }) => {
     const menuLink = document.createElement('div');
-    menuLink.className = 'menu__links';
+    menuLink.className = 'menu__links menu__links--loading';
 
     if (link) {
       const anchor = document.createElement('a');
@@ -58,9 +71,18 @@ function updateMenu() {
       span.textContent = name;
       menuLink.appendChild(span);
     }
+
     fragment.appendChild(menuLink);
+    
+    setTimeout(() => {
+      menuLink.classList.remove('menu__links--loading')
+    }, 100 * delay);
+    
+    delay++
   });
+
   menuLinksContainer.appendChild(fragment);
+ 
 }
 
 openMenuButton.addEventListener('click', () => menu.classList.toggle('menu--active'));
